c2b1b23c55015d0fb6ce3aaa11253e2dde80e6d9
41459392 <41459392@163.com>
2011-07-19 09:51:56 +0800

    initialize TestMyCI project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestMyCI::Application.config.secret_token = 'b90d7f398f58c30f7618ba392d35f38d22fcbd6b93ecc97e5695ab04f8a4e218c7276079975e1b25cf9a2bb1e2c5af7f048b0a67b6a07361d434b8a55c709cce'
